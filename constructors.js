33f0c770c4fc36333ea91e53f664e43c56cd15c8
youngchang8660 <youngchang8660@gmail.com>
2020-02-19 15:03:38 -0700

    completed

--- constructors.js
+++ constructors.js
@@ -15,6 +15,11 @@
 */
 
 // Code here
+function CarFactory(make, model) {
+  this.make = make;
+  this.model =model;
+}
+
 
 ////////// PROBLEM 2 //////////
 
@@ -34,6 +39,7 @@ function Employee(name, email, hireDate) {
   */
   
   // Code here
+  const bob =new Employee('Bob', 'bob@gmail.com', '01-02-98')
   
   ////////// PROBLEM 3 //////////
   
@@ -57,6 +63,18 @@ function Employee(name, email, hireDate) {
   */
   
   // Code here
+  function Car (make, model, year) {
+    this.make = make
+    this.model = model
+    this.year = year
+    this.move = 0
+    this.moveCar = function() {
+      return this.move+=10
+    }
+  }
+
+  
+
   
   ////////// PROBLEM 4 //////////
   
@@ -76,26 +94,60 @@ function Employee(name, email, hireDate) {
   }
   
   // Code here
+  Movie.prototype.changeRating = function(num) {
+    return this.rating + num /2
+  }
   
   ////////// PROBLEM 5 //////////
   
-  // Write a constructor function called User. This function should take in 4 parameters called name, age, email, and savedPosts in that order. Name and email will be strings, age will be a number and savedPosts will be an array of objects. These objects will each have 3 properties: id (a number), title (a string), and rating (a number between 1 and 5). These objects are the posts that the user will have saved to their account.
-  
-  // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
+  // Write a constructor function called User. This function should take in 4 parameters called name, age, email, and savedPosts in that order. 
+  //Name and email will be strings, age will be a number and savedPosts will be an array of objects.
+  // These objects will each have 3 properties: id (a number), title (a string), and rating (a number between 1 and 5). 
+  //These objects are the posts that the user will have saved to their account.
+  // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. 
+  //It should take in three parameters: id (a number), title (a string) and rating (a number). 
+  //Use these parameters to create a new object and 
+  //add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
   
   // Code here
+  function User(name, age, email, savedPosts) {
+    this.name = name
+    this.age = age
+    this.email = email
+    this.savedPosts = savedPosts
+  }
+  User.prototype.addSavedPost = function(id, title, rating) {
+    const newObj = {id, title, rating}
+    this.savedPosts.push(newObj)
+  }
   
   ////////// PROBLEM 6 //////////
   
   // You will be using the constructor function you just created in problem 5.
-  // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
+  // Write a prototype method for the User constructor function named 
+  //removeSavedPost that will take in one number parameter representing the post id. 
+  //Use this id to find and remove the matching object in the savedPosts array.
   
   // Code here
-  
+  User.prototype.removeSavedPost = function(num) {
+    this.savedPosts = this.savedPosts.filter(function(element) {
+      return element.id !== num;
+    })
+  }
+
   ////////// PROBLEM 7 //////////
   
   // You will continue to use the constructor function you created in problem 5.
-  // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
+  // Write a prototype method for the User constructor function named changePostRating that will take in 
+  //two number parameters. The first will be an id (a number) and the second will be the new rating (a number). 
+  //Use the id to find the matching object in the savedPosts array. 
+  //Once you find the matching object, update it's rating score with the new rating parameter.
   
   // Code here
-  
+  User.prototype.changePostRating = function(id, rating) {
+    let foundObj = this.savedPosts.find(function(element) {
+      return element.id === id;
+    })
+    
+    foundObj.rating = rating;
+  }
--- context.js
+++ context.js
@@ -20,13 +20,21 @@
   
 
 //Code Here
-
+let user = {
+  username: 'Young',
+  email: 'young@gmail.com',
+  getUsername: function() {
+    return this.username
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
 
 /*
- Below we have the class Animal.  The eat method is using the "this" keyword.  Use the "new" keyword to assign context to "this", and save the instance to a variable named animal1.  You can pass anything you want in for name, species and food.
+ Below we have the class Animal.  The eat method is using the "this" keyword.  
+ Use the "new" keyword to assign context to "this", and save the instance to a variable named animal1.  
+ You can pass anything you want in for name, species and food.
 */
 
 class Animal {
@@ -42,13 +50,14 @@ class Animal {
 }
 
 //Code Here
-
+const animal1 = new Animal('Soju', 'corgie', 'tuna')
 
 
 ////////// PROBLEM 3 //////////
 
 /*
-  Use the bind method to assign context of the "this" keyword in the sayHi function to the user object; and save the bound function to a variable named whoSaysHi.  
+  Use the bind method to assign context of the "this" keyword in the sayHi function 
+  to the user object; and save the bound function to a variable named whoSaysHi.  
 */
 
 function sayHi(greeting) {
@@ -62,13 +71,14 @@ let who = {
 }
 
 //Code Here
-
+let whoSaysHi = sayHi.bind(who)
 
 
 ////////// PROBLEM 4 //////////
 
 /*
-  here we have a function that just returns the "this" keyword.  We will give context to "this", and your job is to tell us what the context is.
+  here we have a function that just returns the "this" keyword.  
+  We will give context to "this", and your job is to tell us what the context is.
 */
 
 function whatIsThis() {
@@ -76,7 +86,7 @@ function whatIsThis() {
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = window
 
 let product = {
   name: 'snake plant',
@@ -87,7 +97,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -98,7 +108,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 
 class Family {
@@ -116,4 +126,4 @@ class Family {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1
